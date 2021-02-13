ad13f5911d94fcf73daa7b32cfca5522b0bd1445
Allan Gadelha <allan.gadelha@fortics.com.br>
2021-02-13 11:34:59 -0300

    Estruturando projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+const { ApolloServer } = require('apollo-server')
+const graphql = require('./src/graphql')
+
+
+const server = new ApolloServer({
+    ...graphql
+})
+
+server.listen().then(({ url }) => console.log(`🔥 Runing in ${ url } 💪`))
